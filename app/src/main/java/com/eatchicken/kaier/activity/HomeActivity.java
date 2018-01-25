70a10603f901ceeebe452f86f5e27dd1a38704c9
koume <xiaomei.zhou@monstar-lab-cn.com>
2018-01-25 15:29:02 +0800

    refs #create fragment

--- app/src/main/java/com/eatchicken/kaier/activity/HomeActivity.java
+++ app/src/main/java/com/eatchicken/kaier/activity/HomeActivity.java
@@ -4,12 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.eatchicken.kaier.R;
+import com.eatchicken.kaier.widgets.HomeTab;
 
 public class HomeActivity extends Activity {
+    private HomeTab homeTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initView();
+        setEvent();
+    }
+
+    private void setEvent() {
+
+    }
+
+    private void initView() {
+        homeTab = findViewById(R.id.home_tab);
     }
 }
---
+++ app/src/main/java/com/eatchicken/kaier/fragment/WareHouseFragment.java
@@ -0,0 +1,4 @@
+package com.eatchicken.kaier.fragment;
+
+public class WareHouseFragment {
+}
