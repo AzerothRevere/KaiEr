fd035a6175c2355330f500abbd23dffd96adc58c
koume <xiaomei.zhou@monstar-lab-cn.com>
2018-01-29 13:46:49 +0800

    refs #add

--- app/src/main/java/com/eatchicken/kaier/fragment/userFragment.java
+++ app/src/main/java/com/eatchicken/kaier/fragment/userFragment.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 
 import com.eatchicken.kaier.R;
 
-public class UserFragment extends Fragment{
+public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_user, container, false);
